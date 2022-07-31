c3298ee7549df53cc09a62a1cf555cf223051724
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-08-01 00:37:19 +0530

    email template added

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ method    post
 
 Router.post("/mail", async (req, res) => {
     try {
-        const { email } = req.body;
+        const { email,name } = req.body;
         console.log(req.body);
         var transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -26,7 +26,19 @@ Router.post("/mail", async (req, res) => {
             from: 'kanishkagour28@gmail.com',
             to: email,
             subject: 'Yaap Event',
-            text: `Hey!!, You have successfully registered for the yaap event`
+            text: `Hello {name goes here}
+
+            Thank you for signing up to our event! We are really looking forward to
+            having you here.
+            
+            Here is the link to join : https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9 
+            
+            Reply to this email if you have any questions. 
+            We are here to help! 
+            
+            Regards,
+            YAAP`,
+            html: `<strong>Hello ${name}<strong/><br/><br/><p font-size: 14px; font-weight: normal; color: black;>Thank you for signing up to our event! We are really looking forward to having you here.<p/><br/><p>Here is the link to join : <a href="https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9">https://spatial.io/s/Yaap-Main-Venue-62d717a487de560001106ab9</a> <p/><p>Reply to this email if you have any questions. <p/><p>We are here to help! <p/> <br/> <p>Regards,<p/><strong>YAAP<strong/>`
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
